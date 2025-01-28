b5cd6e09ba44716fd0896e20764c8674c1a1988d
Mert Vurgun <vurgun@medienmonster.com>
2025-01-28 18:13:56 +0300

    setup MobileUserExerciseInActionScreenTrophyComponent ui

--- composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/exercises/MobileUserExerciseInActionScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/exercises/MobileUserExerciseInActionScreen.kt
@@ -1,21 +1,42 @@
 package com.vurgun.skyfit.presentation.mobile.features.user.exercises
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.vurgun.skyfit.presentation.shared.components.ButtonSize
+import com.vurgun.skyfit.presentation.shared.components.ButtonState
+import com.vurgun.skyfit.presentation.shared.components.ButtonVariant
+import com.vurgun.skyfit.presentation.shared.components.SkyFitButtonComponent
 import com.vurgun.skyfit.presentation.shared.features.common.TodoBox
 import com.vurgun.skyfit.presentation.shared.navigation.SkyFitNavigationRoute
 import com.vurgun.skyfit.presentation.shared.navigation.jumpAndTakeover
+import com.vurgun.skyfit.presentation.shared.resources.SkyFitColor
+import com.vurgun.skyfit.presentation.shared.resources.SkyFitTypography
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.painterResource
+import skyfit.composeapp.generated.resources.Res
+import skyfit.composeapp.generated.resources.logo_skyfit
 
 private enum class MobileUserExerciseInActionScreenStep {
     SESSION,
@@ -29,7 +50,7 @@ private enum class MobileUserExerciseInActionScreenStep {
 fun MobileUserExerciseInActionScreen(navigator: Navigator) {
 
     val showToolbar: Boolean = false
-    val step = MobileUserExerciseInActionScreenStep.SESSION
+    var activePage by remember { mutableStateOf(MobileUserExerciseInActionScreenStep.TROPHY) }
 
     Box(Modifier.fillMaxSize()) {
         Column(
@@ -37,7 +58,7 @@ fun MobileUserExerciseInActionScreen(navigator: Navigator) {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
 
-            when (step) {
+            when (activePage) {
                 MobileUserExerciseInActionScreenStep.SESSION -> {
                     if (showToolbar) {
                         MobileUserExerciseInActionScreenToolbarComponent()
@@ -62,7 +83,14 @@ fun MobileUserExerciseInActionScreen(navigator: Navigator) {
                 MobileUserExerciseInActionScreenStep.TROPHY -> {
                     MobileUserExerciseInActionScreenToolbarComponent()
                     Spacer(Modifier.height(18.dp))
-                    MobileUserExerciseInActionScreenProgramComponent()
+                    MobileUserExerciseInActionScreenTrophyComponent(
+                        onClickTrophy = {
+                            navigator.jumpAndTakeover(SkyFitNavigationRoute.Dashboard, SkyFitNavigationRoute.UserTrophies)
+                        },
+                        onClickNext = {
+                            activePage = MobileUserExerciseInActionScreenStep.COMPLETE
+                        }
+                    )
                 }
 
                 MobileUserExerciseInActionScreenStep.COMPLETE -> {
@@ -93,8 +121,53 @@ private fun MobileUserExerciseInActionScreenProgramComponent() {
 }
 
 @Composable
-private fun MobileUserExerciseInActionScreenTrophyComponent() {
-    TodoBox("MobileUserExerciseInActionScreenTrophyComponent", Modifier.size(382.dp, 780.dp))
+private fun MobileUserExerciseInActionScreenTrophyComponent(
+    onClickTrophy: () -> Unit,
+    onClickNext: () -> Unit
+) {
+    Box(
+        Modifier.padding(horizontal = 24.dp, vertical = 18.dp)
+            .fillMaxWidth()
+            .background(SkyFitColor.background.surfaceHover, RoundedCornerShape(20.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            Modifier.fillMaxSize().padding(horizontal = 76.dp, vertical = 112.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                Modifier
+                    .size(148.dp, 203.dp)
+                    .background(SkyFitColor.background.surfaceDisabled)
+            )
+            Spacer(Modifier.height(36.dp))
+            Text(
+                text = "3 Günlük Seri Tamamlandı!",
+                textAlign = TextAlign.Center,
+                style = SkyFitTypography.heading3,
+                modifier = Modifier.width(230.dp)
+            )
+            Spacer(Modifier.height(36.dp))
+            SkyFitButtonComponent(
+                Modifier.fillMaxWidth(), text = "Ödüller",
+                onClick = onClickTrophy,
+                variant = ButtonVariant.Secondary,
+                size = ButtonSize.Large,
+                initialState = ButtonState.Rest,
+                leftIconPainter = painterResource(Res.drawable.logo_skyfit)
+            )
+            Spacer(Modifier.height(16.dp))
+            SkyFitButtonComponent(
+                Modifier.fillMaxWidth(), text = "Ileri",
+                onClick = onClickNext,
+                variant = ButtonVariant.Primary,
+                size = ButtonSize.Large,
+                initialState = ButtonState.Rest
+            )
+        }
+    }
 }
 
 @Composable
