ee45ada5dc9ce1de08f172f0210d9501760a5cd9
Mert Vurgun <vurgun@medienmonster.com>
2025-01-29 01:36:24 +0300

    setup MobileUserPhotoDiaryScreenExercisesHeaderComponent ui

--- composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/profile/MobileUserPhotoDiaryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/profile/MobileUserPhotoDiaryScreen.kt
@@ -2,32 +2,43 @@ package com.vurgun.skyfit.presentation.mobile.features.user.profile
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.vurgun.skyfit.presentation.shared.components.SkyFitScreenHeader
 import com.vurgun.skyfit.presentation.shared.features.common.TodoBox
 import com.vurgun.skyfit.presentation.shared.resources.SkyFitColor
+import com.vurgun.skyfit.presentation.shared.resources.SkyFitTypography
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.painterResource
+import skyfit.composeapp.generated.resources.Res
+import skyfit.composeapp.generated.resources.logo_skyfit
 
 @Composable
 fun MobileUserPhotoDiaryScreen(navigator: Navigator) {
 
     var dietsExpanded: Boolean = false
     var exercisesExpanded: Boolean = false
-    var showAddPhoto: Boolean = true
+    var showAddPhoto: Boolean = false
 
     Scaffold(
         backgroundColor = SkyFitColor.background.default,
         topBar = {
-            MobileUserPhotoDiaryScreenToolbarComponent()
+            SkyFitScreenHeader("Fotograf Gunlugum", onBackClick = { navigator.popBackStack() })
         }
     ) {
         Box(Modifier.fillMaxSize()) {
@@ -48,6 +59,11 @@ fun MobileUserPhotoDiaryScreen(navigator: Navigator) {
 
                 MobileUserPhotoDiaryScreenExercisesHeaderComponent()
 
+                Divider(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = SkyFitColor.border.default
+                )
+
                 if (exercisesExpanded) {
                     MobileUserPhotoDiaryScreenExercisesComponent()
                 } else {
@@ -64,13 +80,38 @@ fun MobileUserPhotoDiaryScreen(navigator: Navigator) {
 }
 
 @Composable
-private fun MobileUserPhotoDiaryScreenToolbarComponent() {
-    TodoBox("MobileUserPhotoDiaryScreenToolbarComponent", Modifier.size(430.dp, 40.dp))
-}
-
-@Composable
 private fun MobileUserPhotoDiaryScreenDietsHeaderComponent() {
-    TodoBox("MobileUserPhotoDiaryScreenDietsHeaderComponent", Modifier.size(430.dp, 74.dp))
+    Box(Modifier.fillMaxWidth().padding(16.dp), contentAlignment = Alignment.Center) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(Modifier.weight(1f)) {
+                Row {
+                    Icon(
+                        painter = painterResource(Res.drawable.logo_skyfit),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = SkyFitColor.icon.default
+                    )
+                    Text(
+                        text = "Diyet",
+                        style = SkyFitTypography.bodyLargeSemibold
+                    )
+                }
+                Text(
+                    text = "Çarşamba, 28 Ağustos",
+                    style = SkyFitTypography.bodySmall,
+                    color = SkyFitColor.text.secondary
+                )
+            }
+
+            Icon(
+                painter = painterResource(Res.drawable.logo_skyfit),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp),
+                tint = SkyFitColor.icon.default
+            )
+        }
+    }
 }
 
 @Composable
@@ -85,7 +126,37 @@ private fun MobileUserPhotoDiaryScreenDietsComponent() {
 
 @Composable
 private fun MobileUserPhotoDiaryScreenExercisesHeaderComponent() {
-    TodoBox("MobileUserPhotoDiaryScreenExercisesHeaderComponent", Modifier.size(430.dp, 74.dp))
+    Box(Modifier.fillMaxWidth().padding(16.dp), contentAlignment = Alignment.Center) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(Modifier.weight(1f)) {
+                Row {
+                    Icon(
+                        painter = painterResource(Res.drawable.logo_skyfit),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = SkyFitColor.icon.default
+                    )
+                    Text(
+                        text = "Egzersiz",
+                        style = SkyFitTypography.bodyLargeSemibold
+                    )
+                }
+                Text(
+                    text = "Çarşamba, 28 Ağustos",
+                    style = SkyFitTypography.bodySmall,
+                    color = SkyFitColor.text.secondary
+                )
+            }
+
+            Icon(
+                painter = painterResource(Res.drawable.logo_skyfit),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp),
+                tint = SkyFitColor.icon.default
+            )
+        }
+    }
 }
 
 @Composable
