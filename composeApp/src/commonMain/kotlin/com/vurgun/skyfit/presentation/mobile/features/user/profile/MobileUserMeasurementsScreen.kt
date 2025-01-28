87cefff9610ebc63e8451073e37ba5899d988448
Mert Vurgun <vurgun@medienmonster.com>
2025-01-28 17:29:46 +0300

    setup MeasurementItemComponent ui

--- composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/profile/MobileUserMeasurementsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/profile/MobileUserMeasurementsScreen.kt
@@ -1,43 +1,72 @@
 package com.vurgun.skyfit.presentation.mobile.features.user.profile
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.vurgun.skyfit.presentation.shared.components.SkyFitScreenHeader
 import com.vurgun.skyfit.presentation.shared.features.common.TodoBox
 import com.vurgun.skyfit.presentation.shared.resources.SkyFitColor
+import com.vurgun.skyfit.presentation.shared.resources.SkyFitTypography
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.painterResource
+import skyfit.composeapp.generated.resources.Res
+import skyfit.composeapp.generated.resources.logo_skyfit
 
 @Composable
 fun MobileUserMeasurementsScreen(navigator: Navigator) {
+
+    var measurements = listOf(
+        "YMCA Submaximal Step Testi",
+        "2YMCA Submaximal Step Testi",
+        "3YMCA Submaximal Step Testi"
+    )
 
     Scaffold(
         backgroundColor = SkyFitColor.background.default,
         topBar = {
             Column {
-                MobileUserMeasurementsScreenToolbarComponent()
+                SkyFitScreenHeader(
+                    title = "Ölçümlerim",
+                    onBackClick = { navigator.popBackStack() }
+                )
+                Spacer(Modifier.height(8.dp))
                 MobileUserMeasurementsScreenSearchComponent()
             }
         }
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(top = 24.dp)
         ) {
-            MobileUserMeasurementsComponent()
+            item {
+                Text(
+                    text = "En Son Güncellenen",
+                    style = SkyFitTypography.bodyMediumSemibold,
+                    color = SkyFitColor.text.secondary,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+            items(measurements) {
+                MeasurementItemComponent(it, onClick = {
+
+                })
+            }
         }
     }
-}
-
-@Composable
-private fun MobileUserMeasurementsScreenToolbarComponent() {
-    TodoBox("MobileUserMeasurementsScreenToolbarComponent", Modifier.size(430.dp, 40.dp))
 }
 
 @Composable
@@ -46,6 +75,21 @@ private fun MobileUserMeasurementsScreenSearchComponent() {
 }
 
 @Composable
-private fun MobileUserMeasurementsComponent() {
-    TodoBox("MobileUserMeasurementsComponent", Modifier.size(430.dp, 558.dp))
+private fun MeasurementItemComponent(item: String, onClick: () -> Unit) {
+    Column {
+        Row(Modifier.fillMaxWidth().padding(16.dp)) {
+            Text(
+                text = item,
+                style = SkyFitTypography.bodyMediumSemibold,
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                painter = painterResource(Res.drawable.logo_skyfit),
+                contentDescription = "Enter",
+                tint = SkyFitColor.icon.default,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+        Divider(Modifier.fillMaxWidth().height(1.dp), color = SkyFitColor.border.default)
+    }
 }
--- composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/navigation/MobileNavigationGraph.kt
+++ composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/navigation/MobileNavigationGraph.kt
@@ -90,7 +90,7 @@ fun MobileNavigationGraph() {
 
     NavHost(
         navigator = rootNavigator,
-        initialRoute = SkyFitNavigationRoute.UserActivityCalendarConfirmed.route
+        initialRoute = SkyFitNavigationRoute.UserMeasurements.route
     ) {
 
         // Auth
