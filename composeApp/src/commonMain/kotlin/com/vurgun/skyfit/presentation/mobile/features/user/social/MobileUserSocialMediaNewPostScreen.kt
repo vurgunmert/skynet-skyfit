53782de88673d550fb606cfb2f779492ecdd0440
Mert Vurgun <vurgun@medienmonster.com>
2025-01-30 15:09:08 +0300

    setup post components ui

--- composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/social/MobileUserSocialMediaNewPostScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vurgun/skyfit/presentation/mobile/features/user/social/MobileUserSocialMediaNewPostScreen.kt
@@ -1,17 +1,27 @@
 package com.vurgun.skyfit.presentation.mobile.features.user.social
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.vurgun.skyfit.presentation.shared.features.common.TodoBox
+import com.vurgun.skyfit.presentation.shared.components.ButtonSize
+import com.vurgun.skyfit.presentation.shared.components.ButtonState
+import com.vurgun.skyfit.presentation.shared.components.ButtonVariant
+import com.vurgun.skyfit.presentation.shared.components.SkyFitButtonComponent
+import com.vurgun.skyfit.presentation.shared.components.SkyFitTextInputComponent
 import com.vurgun.skyfit.presentation.shared.resources.SkyFitColor
+import com.vurgun.skyfit.presentation.shared.resources.SkyFitTypography
 import moe.tlaster.precompose.navigation.Navigator
 
 @Composable
@@ -20,7 +30,10 @@ fun MobileUserSocialMediaNewPostScreen(navigator: Navigator) {
     Scaffold(
         backgroundColor = SkyFitColor.background.default,
         topBar = {
-            MobileUserSocialMediaNewPostScreenToolbarComponent()
+            MobileUserSocialMediaNewPostScreenToolbarComponent(
+                onClickShare = {},
+                onClickCancel = {}
+            )
         }
     ) {
         Column(
@@ -33,11 +46,35 @@ fun MobileUserSocialMediaNewPostScreen(navigator: Navigator) {
 }
 
 @Composable
-private fun MobileUserSocialMediaNewPostScreenToolbarComponent() {
-    TodoBox("MobileUserSocialMediaNewPostScreenUserProfilesComponent", Modifier.size(430.dp, 52.dp))
+private fun MobileUserSocialMediaNewPostScreenToolbarComponent(onClickCancel: () -> Unit,
+                                                               onClickShare: () -> Unit) {
+
+    Row(
+        Modifier.fillMaxWidth().padding(horizontal = 24.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            "İptal",
+            style = SkyFitTypography.bodyLarge,
+            modifier = Modifier.clickable(onClick = onClickCancel)
+        )
+        Spacer(Modifier.weight(1f))
+        SkyFitButtonComponent(
+            Modifier.fillMaxWidth(), text = "Paylaş",
+            onClick = onClickShare,
+            variant = ButtonVariant.Primary,
+            size = ButtonSize.Large,
+            initialState = ButtonState.Rest
+        )
+    }
 }
 
 @Composable
 private fun MobileUserSocialMediaNewPostScreenInputComponent() {
-    TodoBox("MobileUserSocialMediaNewPostScreenInputComponent", Modifier.size(398.dp, 480.dp))
+    SkyFitTextInputComponent(
+        hint = "Post content filling",
+        value = "5 km koşu ve ardından 20 dakika core çalışması! Hedefime adım adım yaklaşıyorum \uD83D\uDCAA",
+    )
+
 }
